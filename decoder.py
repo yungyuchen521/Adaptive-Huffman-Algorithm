d52b429a9593df72816c25dff3f947f8977e4921
yungyuchen521 <yungyuchen521@gmail.com>
2023-03-27 10:44:02 +0800

    encode / decode with proper io

--- decoder.py
+++ decoder.py
@@ -1,10 +1,7 @@
 from typing import Optional
-from io import TextIOWrapper
-
-from utils import (
-    BITS_PER_BYTE,
-    BUFFER_SIZE,
-)
+from io import BufferedReader
+ 
+from bit_io_stream import BitInStream, BitOutStream, IO_MODE_BIT, IO_MODE_BYTE, BITS_PER_BYTE
 from huffman_tree import HuffmanTree
 
 
@@ -20,26 +17,52 @@ class Decoder:
         if decomp_file_path is None:
             decomp_file_path = f"{src_file_path}.decomp"
 
-        with open(src_file_path, "r", BUFFER_SIZE) as src, open(decomp_file_path, "w", BUFFER_SIZE) as decomp:
+        with open(src_file_path, "rb") as src, open(decomp_file_path, "wb") as decomp:
             self._parse_header(src)
 
-            bits = src.read()
-            for order in self._tree.decode(bits):
-                symbol = chr(order)
-                decomp.write(symbol)
+            istream = BitInStream(src, mode=IO_MODE_BIT)
+            ostream = BitOutStream(decomp, mode=IO_MODE_BYTE)
 
-    def _parse_header(self, file_obj: TextIOWrapper):
-        self._bits_per_symbol = ord(file_obj.read(1))
+            next_bit_seq = self._get_bit_seuence(istream)
+            while next_bit_seq:
+                curr_bit_seq = next_bit_seq
+                next_bit_seq = self._get_bit_seuence(istream)
+
+                if not next_bit_seq and self._dummy_codeword_bits > 0:
+                    curr_bit_seq = curr_bit_seq[:-self._dummy_codeword_bits]
+
+                for order in self._tree.decode(curr_bit_seq):
+                    symbol = chr(order)
+                    ostream.write(symbol)
+
+    def _parse_header(self, file_obj: BufferedReader):
+        # {bits per symbol}{dummy symbol bytes}{code length table}{dummy codeword bits}
+
+        stream = BitInStream(file_obj, mode=IO_MODE_BYTE)
+
+        self._bits_per_symbol = stream.read()
         self.BYTES_PER_READ = self._bits_per_symbol // BITS_PER_BYTE
         
-        self._dummy_symbol_bytes = ord(file_obj.read(1))
+        self._dummy_symbol_bytes = stream.read()
 
         code_len_table = [
-            ord(file_obj.read(1)) 
+            stream.read() 
             for _ in range(2 ** self._bits_per_symbol)
         ]
 
-        self._dummy_codeword_bits = ord(file_obj.read(1))
+        self._dummy_codeword_bits = stream.read()
 
         self._tree = HuffmanTree(code_len_table=code_len_table)
         self._code_dict = self._tree.code_dict  # for debug purpose
+
+    def _get_bit_seuence(self, stream: BitInStream) -> str:
+        seq = ""
+        for _ in range(BITS_PER_BYTE):
+            bit = stream.read()
+            if bit == BitInStream.EOF:
+                return ""
+            
+            seq += str(bit)
+
+        assert len(seq) == BITS_PER_BYTE
+        return seq
--- encoder.py
+++ encoder.py
@@ -1,15 +1,12 @@
 from typing import Optional, Dict
 
-from utils import (
-    BITS_PER_BYTE,
-    BUFFER_SIZE,
-)
+from bit_io_stream import BitInStream, BitOutStream, IO_MODE_BYTE, IO_MODE_BIT, BITS_PER_BYTE
 from huffman_tree import HuffmanTree
 
 
 class Encoder:
     def __init__(self, bits_per_symbol: int):
-        assert 0 < bits_per_symbol < 2**BITS_PER_BYTE and bits_per_symbol % 8 == 0
+        assert 0 < bits_per_symbol < 2**BITS_PER_BYTE and bits_per_symbol % BITS_PER_BYTE == 0
 
         self._bits_per_symbol: int = bits_per_symbol
         self.BYTES_PER_READ = self._bits_per_symbol // BITS_PER_BYTE
@@ -29,61 +26,70 @@ class Encoder:
                 pass
             return
         elif len(self._symbol_distributions) == 1:
-            return
-
-        self._tree = HuffmanTree(symbol_distribution=self._symbol_distributions)
-        self._code_dict = self._tree.code_dict
+            self._code_dict = { order: "0" for order, _ in self._symbol_distributions.items() }
+        else:
+            self._tree = HuffmanTree(symbol_distribution=self._symbol_distributions)
+            self._code_dict = self._tree.code_dict
 
         self._write_header(self._code_dict, comp_file_path)
         self._write_content(self._code_dict, src_file_path, comp_file_path)
 
     def _generate_symbol_dist(self, src_file_path: str):
-        with open(src_file_path, "r", BUFFER_SIZE) as f:
+        with open(src_file_path, "rb") as f:
+            stream = BitInStream(f, IO_MODE_BYTE)
+
             while True:
-                symbol = f.read(self.BYTES_PER_READ)
-
-                if len(symbol) == 0:
+                symbol_order = stream.read()
+                if symbol_order == BitInStream.EOF:
                     break
-                elif len(symbol) < self.BYTES_PER_READ:
-                    self._dummy_symbol_bytes = self.BYTES_PER_READ - len(symbol)
-                    symbol += chr(0) * self._dummy_symbol_bytes
+                # elif len(symbol) < self.BYTES_PER_READ:
+                #     self._dummy_symbol_bytes = self.BYTES_PER_READ - len(symbol)
+                #     symbol += chr(0) * self._dummy_symbol_bytes
 
-                order = ord(symbol)
-                if order in self._symbol_distributions:
-                    self._symbol_distributions[order] += 1
+                if symbol_order in self._symbol_distributions:
+                    self._symbol_distributions[symbol_order] += 1
                 else:
-                    self._symbol_distributions[order] = 1
+                    self._symbol_distributions[symbol_order] = 1
 
     def _write_header(self, code_dict: Dict[int, str], comp_file_path: str):
         # {bits per symbol}{dummy symbol bytes}{code length table}{dummy codeword bits}
 
-        with open(comp_file_path, "w", BUFFER_SIZE) as f:
-            f.write(chr(self._bits_per_symbol))
-            f.write(chr(self._dummy_symbol_bytes))
+        with open(comp_file_path, "wb") as f:
+            stream = BitOutStream(f, mode=IO_MODE_BYTE)
+
+            stream.write(chr(self._bits_per_symbol))
+            stream.write(chr(self._dummy_symbol_bytes))
 
             trailing_bits = 0  # bits insufficient to make a byte
             for order in range(2 ** self._bits_per_symbol):
                 code_len = len(code_dict.get(order, ""))
-                
-                f.write(chr(code_len))
-
                 if code_len > 0:
                     trailing_bits += code_len * self._symbol_distributions[order]
                     trailing_bits %= BITS_PER_BYTE
 
+                stream.write(chr(code_len))
+
             self._dummy_codeword_bits = (BITS_PER_BYTE - trailing_bits) % BITS_PER_BYTE
-            f.write(chr(self._dummy_codeword_bits))
+            stream.write(chr(self._dummy_codeword_bits))
 
     def _write_content(self, code_dict: Dict[int, str], src_file_path: str, comp_file_path: str):
-        with open(src_file_path, "r", BUFFER_SIZE) as src, open(comp_file_path, "a", BUFFER_SIZE) as comp:
+        with open(src_file_path, "rb") as src, open(comp_file_path, "ab") as comp:
+            istream = BitInStream(src, mode=IO_MODE_BYTE)
+            ostream = BitOutStream(comp, mode=IO_MODE_BIT)
+
             while True:
-                symbol = src.read(1)
+                symbol_order = istream.read()
 
-                if len(symbol) == 0:
+                if symbol_order == BitInStream.EOF:
                     break
-                elif len(symbol) < self.BYTES_PER_READ:
-                    assert self._dummy_symbol_bytes == self.BYTES_PER_READ - len(symbol)
-                    symbol += chr(0) * self._dummy_symbol_bytes
+                # elif len(symbol) < self.BYTES_PER_READ:
+                #     assert self._dummy_symbol_bytes == self.BYTES_PER_READ - len(symbol)
+                #     symbol += chr(0) * self._dummy_symbol_bytes
 
-                order = ord(symbol)
-                comp.write(code_dict[order])
+                code_word = code_dict[symbol_order]
+                for c in code_word:
+                    ostream.write(c)
+
+            trailing_bits = ostream.flush()
+            dummy_bits = 0 if trailing_bits == 0 else BITS_PER_BYTE - trailing_bits
+            assert self._dummy_codeword_bits == dummy_bits
